de638db7f318c1b6045819e6c02a6cda7b7a132d
Chetphisuth Tongpa <chetphisuth.t@ku.th>
2024-11-07 14:42:02 +0700

    Changed to OOP + finish all the methods

--- polygon_art.py
+++ polygon_art.py
@@ -3,56 +3,65 @@ import random
 
 class Canvas:
 
-    def __init__(self, speed: int = 0, 
-                 bgcolor: str='black', 
-                 tracer: int = 0, 
+    def __init__(self, speed: int = 0,
+                 bgcolor: str = 'black',
+                 tracer: int = 0,
                  colormode: int = 255):
+        """
+        Create the canvas with the given values
+
+        Args:
+            speed (int): How fast to draw, 10-1 for fast-slow, 0 for fastest.
+            bgcolor (str): The bankground color of the canvas.
+            tracer (int): Set delay for update drawings, must be a non-negative integer.
+            colormode (int): Set the RGB value scale, 1.0 or 255.
+        """
         turtle.speed(speed)
         turtle.bgcolor(bgcolor)
         turtle.tracer(tracer)
         turtle.colormode(colormode)
-        self.__range_layers = [1,1]
-        self.__range_num_sides = [3,6]
-        self.__range_num_shapes = [30,40]
-        self.randomize_values()
-
-    def randomize_values(self):
-        self.__layers = random.randint(self.__range_layers[0],self.__range_layers[1])
-        self.__num_sides = random.randint(self.__range_num_sides[0], self.__range_num_sides[1])
-        self.__num_shapes = random.randint(self.__range_num_shapes[0],self.__range_num_shapes[1])
-
-        self.__color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
-        self.__location = [random.randint(-300, 300), random.randint(-200, 200)]
-        self.__size = random.randint(50, 150)
-        self.__orientation = random.randint(0, 90)
-        self.__location = [random.randint(-300, 300), random.randint(-200, 200)]
-        self.__border_size = random.randint(1, 10)
-        self.__reduction_ratio = 0.618
 
     def melody(self,
-                 range_layers : list, 
-                 range_num_sides : list,
-                 range_num_shapes : list):
+               range_num_layers: list,
+               range_num_sides: list,
+               range_num_polygon: list):
+        """
+        Create the artwork with the given randomize range of layers, sides and shapes.
+        (the artist's melody)
 
-        self.__range_layers = range_layers
+        Args:
+            range_num_layers (list): Range for randomizing number of indented polygon layers. 
+                                    ex. [a, b] = from a layers to b layers.
+            range_num_sides (list): Range for randomizing number of polygon sides.
+                                    ex. [a, b] = from a sides to b sides.
+            range_num_polygon (list): Range for randomizing number of polygon.
+                                    ex. [a, b] = from a polygons to b polygons.
+        """
+        self.__range_num_layers = range_num_layers
         self.__range_num_sides = range_num_sides
-        self.__range_num_shapes = range_num_shapes
+        self.__range_num_polygon = range_num_polygon
 
+        self.randomize_values()
         for _ in range(self.__num_shapes):
             self.randomize_values()
-            self.setup_shape()
-            for _ in range(self.__layers):
-                self.draw()
+            self.setup_polygon()
+            for _ in range(self.__num_layers):
+                self.draw_polygon()
                 self.move_to_next_layer()
 
-    def setup_shape(self):
+    def setup_polygon(self):
+        """
+        Set the color, location, orientation and the border size 
+        of the shape before drawing.
+        """
         turtle.penup()
         turtle.goto(self.__location[0], self.__location[1])
         turtle.setheading(self.__orientation)
         turtle.color(self.__color)
         turtle.pensize(self.__border_size)
 
-    def draw(self):
+    def draw_polygon(self):
+        """ Draw the polygon with the indicated shape onto the canvas."""
         turtle.pendown()
         for _ in range(self.__num_sides):
             turtle.forward(self.__size)
@@ -60,6 +69,7 @@ class Canvas:
         turtle.penup()
 
     def move_to_next_layer(self):
+        """ Go reposition into the inner layer of the polygon."""
         self.__size *= self.__reduction_ratio
         turtle.penup()
         turtle.forward(self.__size*(1-self.__reduction_ratio)/2)
@@ -67,21 +77,68 @@ class Canvas:
         turtle.forward(self.__size*(1-self.__reduction_ratio)/2)
         turtle.right(90)
 
+    def randomize_values(self):
+        """ Randomize all the value to get ready to draw the next shape."""
+        self.__num_layers = random.randint(self.__range_num_layers[0], self.__range_num_layers[1])
+        self.__num_sides = random.randint(self.__range_num_sides[0], self.__range_num_sides[1])
+        self.__num_shapes = random.randint(self.__range_num_polygon[0], self.__range_num_polygon[1])
+
+        self.__color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+        self.__location = [random.randint(-250, 250), random.randint(-170, 170)]
+        self.__size = random.randint(50, 150)
+        self.__orientation = random.randint(0, 90)
+        self.__border_size = random.randint(1, 10)
+        self.__reduction_ratio = 0.618
+    
+    def clear(self):
+        """ Clear the paintings from the canvas"""
+        turtle.clear()
 
 
-# draw a polygon at a random location, orientation, color, and border line thickness
+class Painter:
+
+    def __init__(self, canvas: Canvas = Canvas()) -> None:
+        """
+        A painter
+        
+        Args:
+            canvas (Canvas): Canvas that the painter will be painting on.
+        """
+        self.__canvas = canvas
+
+    def hear(self):
+        """ Tell Painter what to do, will ask for an input"""
+        self.__task_set = int(input("Which art do you want to generate? Enter a number between 1 to 9 inclusive: "))
+        self.brain()
+
+    def brain(self):
+        """ Painter's vital organs for processing given task and will paint accordingly"""
+        if self.__task_set == 0:
+            self.__canvas.clear()
+            return
+
+        if self.__task_set % 4 != 0 and self.__task_set != 9:
+            range_num_sides = [self.__task_set + 2, self.__task_set + 2]
+        else:
+            range_num_sides = [3, 5]
+        
+        if self.__task_set <= 4:
+            range_num_layers = [1, 1]
+        elif self.__task_set <= 8:
+            range_num_layers = [3, 3]
+        else:
+            range_num_layers = [1, 3]
+        
+        range_num_polygon = [20, 30]
+        self.__canvas.melody(range_num_layers, range_num_sides, range_num_polygon)
 
 
-# specify a reduction ratio to draw a smaller polygon inside the one above
+# Main
+Josh = Painter()
+# Josh.hear()
 
-# reposition the turtle and get a new location
+# # Additionally I made it so that you can paint multiple times (type 0 to clear the canvas)
+# while(1):
+#     Josh.hear()
 
-
-c = Canvas()
-c.melody([1,4], [3,4], [10,10])
-# adjust the size according to the reduction ratio
-
-# draw the second polygon embedded inside the original 
-
-# hold the window; close it by clicking the window close 'x' mark
 turtle.done()
